3ea6b9be406a0c7644b21f316565a1c396cc6c99
Meifa Ratna <meifa10@example.com>
2026-04-05 19:47:38 +0700

    push5

--- routes/web.php
+++ routes/web.php
@@ -101,8 +101,12 @@ Route::middleware('auth')->group(function () {
 |--------------------------------------------------------------------------
 */
 
+Route::middleware('auth')->group(function(){
+
 Route::get('/payment/{id}', [PaymentController::class,'pay'])
     ->name('payment.pay');
+
+});
 
 // Route::post('/payment/callback', [CallbackController::class, 'handle']);
 Route::get('/payment/finish', function () {
